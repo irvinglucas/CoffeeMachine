ec450c8f94909620fc71eb8b01e0a0c13bc62574
Irving Lucas <irving.lucas@gmail.com>
2022-06-13 01:27:36 -0300

    Finishing the stage 4/6.

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,27 +1,113 @@
 package machine
 
-const val DEFAULT_WATER_ML = 200
-const val DEFAULT_MILK_ML = 50
-const val DEFAULT_BEANS_GRAMS = 15
+// espresso needed supplies
+const val ESPRESSO_WATER = 250 // in ml
+//const val ESPRESSO_MILK = 0 // in ml
+const val ESPRESSO_BEANS = 16 //in grams
+const val ESPRESSO_COST = 4 // in dollars
+
+// latte needed supplies
+
+const val LATTE_WATER = 350
+const val LATTE_MILK = 75
+const val LATTE_BEANS = 20
+const val LATTE_COST = 7
+
+// cappuccino needed supplies
+const val CAPPUCCINO_WATER = 200
+const val CAPPUCCINO_MILK = 100
+const val CAPPUCCINO_BEANS = 12
+const val CAPPUCCINO_COST = 6
+
+// coffee machine supplies according to task description
+private var amountOfWater = 400
+private var amountOfMilk = 540
+private var amountOfCoffeeBeans = 120
+private var amountOfDisposableCups = 9
+private var amountOfMoney = 550
+
 
 fun main() {
-    print("Write how many ml of water the coffee machine has: > ")
-    val quantityOfWater = readln().toInt()
-    print("Write how many ml of milk the coffee machine has: > ")
-    val quantityOfMilk = readln().toInt()
-    print("Write how many grams of coffee beans the coffee machine has: >")
-    val gramsOfCoffeeBeans = readln().toInt()
-    print("Write how many cups of coffee will you need: >")
-    val quantityOfCups = readln().toInt()
 
-    val maxCupsWater = quantityOfWater / DEFAULT_WATER_ML
-    val maxCupsMilk = quantityOfMilk / DEFAULT_MILK_ML
-    val maxCupsBeans = gramsOfCoffeeBeans / DEFAULT_BEANS_GRAMS
+    // this function prints out the machine state using print() function
+    getMachineState()
+    print("Write action (buy, fill, take): >")
+    when(readln()) {
+        "buy" -> buy()
+        "fill" -> fill()
+        "take" -> take()
+    }
+}
 
-    val maxCups = minOf(maxCupsWater, maxCupsMilk, maxCupsBeans)
+// operation functions
+// these functions for now only need to print out some text
 
-    if (quantityOfCups == maxCups) println("Yes, I can make that amount of coffee")
-    else if (quantityOfCups < maxCups) println("Yes, I can make that amount of coffee( and even " + (maxCups-quantityOfCups) + "more than that")
-    else println("No, I can make only $maxCups cups of coffee")
+fun getMachineState () {
+    println("""The coffee machine has:
+        |$amountOfWater ml of water
+        |$amountOfMilk ml of milk
+        |$amountOfCoffeeBeans g of coffee beans
+        |$amountOfDisposableCups disposable cups
+        |$$amountOfMoney of money
+    """.trimMargin())
+}
+fun buy () {
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: >")
+    when (readln().toInt()) {
+        1 -> getEspresso()
+        2 -> getLatte()
+        3 -> getCappuccino()
+    }
+}
 
+fun getEspresso () {
+    if ((amountOfWater >= ESPRESSO_WATER) && (amountOfCoffeeBeans >= ESPRESSO_BEANS) && (amountOfDisposableCups >= 1)){
+        amountOfWater -= ESPRESSO_WATER
+        amountOfCoffeeBeans -= ESPRESSO_BEANS
+        amountOfDisposableCups -= 1
+        amountOfMoney += ESPRESSO_COST
+    }
+    getMachineState()
+}
+
+fun getLatte () {
+    if ((amountOfWater >= LATTE_WATER) && (amountOfMilk >= LATTE_MILK) && (amountOfCoffeeBeans >= LATTE_BEANS) && (amountOfDisposableCups >= 1)){
+        amountOfWater -= LATTE_WATER
+        amountOfMilk -= LATTE_MILK
+        amountOfCoffeeBeans -= LATTE_BEANS
+        amountOfDisposableCups -= 1
+        amountOfMoney += LATTE_COST
+    }
+    getMachineState()
+}
+
+fun getCappuccino () {
+
+    if ((amountOfWater >= CAPPUCCINO_WATER) && (amountOfMilk >= CAPPUCCINO_MILK) && (amountOfCoffeeBeans >= CAPPUCCINO_BEANS) && (amountOfDisposableCups >= 1)) {
+        amountOfWater -= CAPPUCCINO_WATER
+        amountOfMilk -= CAPPUCCINO_MILK
+        amountOfCoffeeBeans -= CAPPUCCINO_BEANS
+        amountOfDisposableCups -= 1
+        amountOfMoney += CAPPUCCINO_COST
+    }
+    getMachineState()
+}
+
+fun fill () {
+    print("Write how many ml of water do you want to add: >")
+    amountOfWater += readln().toInt()
+    print("Write how many ml of milk do you want to add: >")
+    amountOfMilk += readln().toInt()
+    print("Write how many grams of coffee beans do you want to add: >")
+    amountOfCoffeeBeans += readln().toInt()
+    print("Write how many disposable cups of coffee do you want to add: >")
+    amountOfDisposableCups += readln().toInt()
+
+    getMachineState()
+}
+
+fun take () {
+    println("I gave you $amountOfMoney")
+    amountOfMoney = 0
+    getMachineState()
 }
